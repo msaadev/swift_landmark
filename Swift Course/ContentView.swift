941ffcd2c9ac591082fa41c8d99d7b85746356d3
samil <studi.msa@gmail.com>
2023-02-06 23:42:18 +0300

    Initial Commit

---
+++ Swift Course/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Swift Course
+//
+//  Created by muhammed samil demir on 6.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Course/Swift_CourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_CourseApp.swift
+//  Swift Course
+//
+//  Created by muhammed samil demir on 6.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Swift_CourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
